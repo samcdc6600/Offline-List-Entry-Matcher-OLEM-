01adf5a3b11f3d83fedd0dac6dd7eb636404cc97
CyanStar <ntpower7000@gmail.com>
2026-01-21 15:17:48 +1100

    Did some more work!

--- superScript.js
+++ superScript.js
@@ -1,9 +1,33 @@
 
 function main()
 {
-    console.log("Start!");
-    // const div = document.getElementById("myDiv");
-    // div.textContent = "Hello";
+    const openFileButton = document.getElementById("openFileButtonId");
+    const fileContent = document.getElementById("candidateList")
+
+
+    openFileButton.addEventListener('click', () => {
+	// Create a hidden file input
+	const input = document.createElement('input');
+	input.type = 'file';
+
+	// When a file is selected
+	input.addEventListener('change', () => {
+	    if (input.files.length > 0) {
+		const file = input.files[0];
+		const reader = new FileReader();
+
+		reader.onload = (e) => {
+		    fileContent.textContent = e.target.result;
+		};
+
+		reader.readAsText(file); // or readAsArrayBuffer / readAsDataURL
+	    }
+	});
+
+	input.click();
+    });
+
+    id="candidateList"
 }
 
 
